672baf06f00ad9c79983f0876000365a6d16fa29
alt-potato <jason.why.zhuang@gmail.com>
2025-10-09 18:40:19 -0400

    Add error handling to item search

--- Frontend/app/components/LogoTile.jsx
+++ Frontend/app/components/LogoTile.jsx
@@ -3,7 +3,7 @@ import Tile from "./Tile";
 
 export default function LogoTile({ children, className = "" }) {
   return (
-    <Tile className={`container mx-auto mt-16 ${className}`}>
+    <Tile className="container mx-auto mt-16">
       <div className="container flex flex-col items-center sm:flex-row sm:items-end gap-8 p-4">
         <img
           src={hedgehog}
@@ -14,7 +14,7 @@ export default function LogoTile({ children, className = "" }) {
         <h1 className="text-6xl font-bold">TradeHub</h1>
       </div>
 
-      <div className="container flex p-4">{children}</div>
+      <div className={`container flex p-4 ${className}`}>{children}</div>
     </Tile>
   );
 }
