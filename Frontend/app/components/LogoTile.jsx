1c833c086e1a2a516449e3f3cf4a0c863b25bad5
alt-potato <jason.why.zhuang@gmail.com>
2025-10-09 22:00:31 -0400

    Update hedgehog logo

--- Frontend/app/components/LogoTile.jsx
+++ Frontend/app/components/LogoTile.jsx
@@ -7,11 +7,11 @@ export default function LogoTile({ children, className = "" }) {
       <div className="container flex flex-col items-center sm:flex-row sm:items-end gap-8 p-4">
         <img
           src={hedgehog}
-          alt="Hedgehog"
-          className="hedgehog-logo"
-          width="250"
+          alt="TradeHub Hedgehog"
+          className="drop-shadow-lg"
+          width="128"
         />
-        <h1 className="text-4xl font-bold text-white drop-shadow-lg">TradeHub</h1>
+        <h1 className="text-6xl font-bold text-white drop-shadow-lg">TradeHub</h1>
       </div>
 
       <div className={`container flex p-4 ${className}`}>{children}</div>
